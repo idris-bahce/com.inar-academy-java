7654ba5d07b3e784150f494247173db6cd6453bd
idris <idrisbahce26@gmail.com>
2022-09-26 06:53:03 +0300

    Java

--- Java/src/chapters/chapter06/listings06/FirsSimpleMethod.java
+++ Java/src/chapters/chapter06/listings06/FirsSimpleMethod.java
@@ -1,2 +1,17 @@
-package chapters.chapter06.listings06;public class FirsSimpleMethod {
+package chapters.chapter06.listings06;
+
+public class FirsSimpleMethod {
+    public static void main(String[] args) {
+        System.out.println(sum(1,10));
+        System.out.println(sum(20,37));
+        System.out.println(sum(35,49));
+
+    }
+    public static int sum(int a , int b){
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
 }
