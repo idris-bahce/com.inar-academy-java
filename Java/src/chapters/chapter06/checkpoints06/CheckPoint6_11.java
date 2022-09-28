3305b74fb03ce85c1bcda1a4efa8a6f4c0335bf3
idris <idrisbahce26@gmail.com>
2022-09-28 06:46:38 +0300

    checkpoints

---
+++ Java/src/chapters/chapter06/checkpoints06/CheckPoint6_11.java
@@ -0,0 +1,7 @@
+package chapters.chapter06.checkpoints06;
+
+public class CheckPoint6_11 {
+    //In the stack, while passing an argument to parameter, there would be a new memory.
+    //that memory has new parameters. and argument can have same name as its parameter.
+    //Because it is in a different memory stack.
+}
---
+++ Java/src/chapters/chapter06/checkpoints06/CheckPoint6_13.java
@@ -0,0 +1,4 @@
+package chapters.chapter06.checkpoints06;
+
+public class CheckPoint6_13 {
+}
