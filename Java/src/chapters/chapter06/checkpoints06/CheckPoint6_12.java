b5525852f1acbff31513fb1bd3f9118e3e73e0d2
idris <idrisbahce26@gmail.com>
2022-09-28 07:10:44 +0300

    checkpoints

--- Java/src/chapters/chapter06/checkpoints06/CheckPoint6_12.java
+++ Java/src/chapters/chapter06/checkpoints06/CheckPoint6_12.java
@@ -1,4 +1,12 @@
 package chapters.chapter06.checkpoints06;
 
 public class CheckPoint6_12 {
+    public static void main(String[] args) {
+        nPrintln("Welcome to Java!",5 );
+    }
+
+    public static void nPrintln(String message, int n) {
+        for (int i = 0; i < n; i++)
+            System.out.println(message);
+    }
 }
--- Java/src/chapters/chapter06/checkpoints06/CheckPoint6_13.java
+++ Java/src/chapters/chapter06/checkpoints06/CheckPoint6_13.java
@@ -1,4 +1,82 @@
 package chapters.chapter06.checkpoints06;
 
 public class CheckPoint6_13 {
+    public static void main(String[] args) {
+        a();
+        b();
+        c();
+        d();
+    }
+    private static void a() {
+        System.out.println("a()\n");
+        int max = 0;
+        max(1, 2, max);
+        System.out.println(max);
+    }
+
+    public static void max(
+            int value1, int value2, int max) {
+        if (value1 > value2)
+            max = value1;
+        else
+            max = value2;
+    }
+
+    private static void b() {
+        System.out.println("b()\n");
+        int i = 1;
+        while (i <= 6) {
+            method1(i, 2);
+            i++;
+        }
+    }
+
+    public static void method1(
+            int i, int num) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(num + " ");
+            num *= 2;
+        }
+        System.out.println();
+    }
+
+    private static void c() {
+        System.out.println("c()\n");
+        // Initialize times
+        int times = 3;
+        System.out.println("Before the call,"
+                + " variable times is " + times);
+        // Invoke nPrintln and display times
+        nPrintln("Welcome to Java!", times);
+        System.out.println("After the call,"
+                + " variable times is " + times);
+    }
+
+        // Print the message n times
+    public static void nPrintln(
+            String message, int n) {
+        while (n > 0) {
+            System.out.println("n = " + n);
+            System.out.println(message);
+            n--;
+        }
+    }
+    private static void d() {
+        System.out.println("d();\n");
+        int i = 0;
+        while (i <= 4) {
+            method1(i);
+            i++;
+        }
+        System.out.println("i is " + i);
+    }
+    public static void method1(int i) {
+        do {
+            if (i % 3 != 0)
+                System.out.print(i + " ");
+            i--;
+        }
+        while (i >= 1);
+        System.out.println();
+    }
 }
