88fbb56346ff8f0abdadee8f9afb012fe576c170
idris <idrisbahce26@gmail.com>
2022-09-29 13:32:08 +0300

    exercise

--- Java/src/chapters/chapter06/excercises06/Excercise6_01.java
+++ Java/src/chapters/chapter06/excercises06/Excercise6_01.java
@@ -1,4 +1,17 @@
 package chapters.chapter06.excercises06;
 
 public class Excercise6_01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.printf("%8d",getPentegonalNum(i));
+            if (i % 10 == 0){
+                System.out.println();
+            }
+        }
+    }
+
+    private static int getPentegonalNum(int n) {
+        int result = n * (3 * n - 1)/2;
+        return result;
+    }
 }
