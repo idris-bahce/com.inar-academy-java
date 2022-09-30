de434378e30e06bace38d87d7b32bd3c4ec813a5
idris <idrisbahce26@gmail.com>
2022-09-30 07:03:18 +0300

    exercises

--- Java/src/chapters/chapter06/excercises06/Exercise6_12.java
+++ Java/src/chapters/chapter06/excercises06/Exercise6_12.java
@@ -2,10 +2,18 @@ package chapters.chapter06.excercises06;
 
 public class Exercise6_12 {
     public static void main(String[] args) {
-
+        printChars('1', 'Z',10);
     }
     public static void printChars(char ch1, char ch2, int numberPerLine){
-        
+        int liner = 1;
+        for (int i = (int)ch1; i < (int)ch2; i++) {
+            char ch = (char)i;
+            System.out.print(ch + " ");
+            if (liner % numberPerLine == 0)
+                System.out.println();
+            liner++;
+        }
+
     }
 
 }
