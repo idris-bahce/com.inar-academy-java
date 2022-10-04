d20e954e569cfc0a0d78076bbd02aaed79266d61
idris <idrisbahce26@gmail.com>
2022-10-04 07:01:08 +0300

    arrays

---
+++ Java/src/chapters/chapter07/listings/ObservingMemory.java
@@ -0,0 +1,12 @@
+package chapters.chapter07.listings;
+
+import java.util.Scanner;
+
+public class ObservingMemory {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = in.nextInt();
+        int[] arr = new int [size];
+    }
+}
