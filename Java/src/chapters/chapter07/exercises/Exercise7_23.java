21e8d62e672a8bb87d1000ea59946fc52263fc1c
idris <idrisbahce26@gmail.com>
2022-10-11 07:30:13 +0300

    exc

--- Java/src/chapters/chapter07/exercises/Exercise7_23.java
+++ Java/src/chapters/chapter07/exercises/Exercise7_23.java
@@ -1,30 +1,27 @@
 package chapters.chapter07.exercises;
-//Unfinished
+
 public class Exercise7_23 {
     public static void main(String[] args) {
-        boolean[] lockers = new boolean[100];
-        for (int j = 0; j < lockers.length; j++) {//student 1
-            lockers[j] = true;
-        }
-        for (int j = 1; j < lockers.length; j++) {//student 2
-            lockers[j] = false;
-        }
-        for (int i = 1; i < lockers.length; i++) {//student 3 and so on...
-            for (int j = i + 1; j < lockers.length; j++) {
-                if (j %( i + 2) == 0){
-                    if (!lockers[i]) {
-                        lockers[i] = true;
-                    }else {
-                        lockers[i] = false;
-                    }
-                }
-            }
-        }
+        boolean[] lockers = new boolean[100]; // true = open; false = closed;
+        whichIsOpen(lockers);
+        printTheOpenLockers(lockers);
+    }
+
+    public static void printTheOpenLockers(boolean[] lockers) {
+        System.out.println("Oen lockers are: ");
         for (int i = 0; i < lockers.length; i++) {
-            if (lockers[i]) {
-                System.out.print(i + " ");;
+            if (lockers[i]){
+                System.out.print(i + " ");
             }
         }
     }
 
+    public static void whichIsOpen(boolean[] lockers) {
+        for (int students = 1; students <= 100; students++) {
+            for (int lockerNum = students-1; lockerNum < lockers.length; lockerNum+=students) {
+                lockers[lockerNum]=!lockers[lockerNum];
+            }
+        }
+
+    }
 }
