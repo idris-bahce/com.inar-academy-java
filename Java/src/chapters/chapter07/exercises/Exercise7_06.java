d139ae4718f11cab3d8f45793dbf27e3582af8b6
idris <idrisbahce26@gmail.com>
2022-10-11 06:59:03 +0300

    exc

--- Java/src/chapters/chapter07/exercises/Exercise7_06.java
+++ Java/src/chapters/chapter07/exercises/Exercise7_06.java
@@ -1,45 +1,42 @@
 package chapters.chapter07.exercises;
-//Unfinished
+
 public class Exercise7_06 {
     public static void main(String[] args) {
         final int NUMBER_OF_PRIMES = 50;
-
-        int[] primeNumbers = new int[NUMBER_OF_PRIMES];
-        fillArray(primeNumbers);
-        displayArray(primeNumbers);
+        int[] primeNum = new int[NUMBER_OF_PRIMES];
+        getThePrimeNumbers(primeNum,NUMBER_OF_PRIMES);
+        printThePrimeNumbers(primeNum);
     }
 
-    public static void displayArray(int[] primeNumbers) {
-        for (int i = 0; i < primeNumbers.length; i++) {
-            if (i % 10 == 0)
+    private static void printThePrimeNumbers(int[] primeNum) {
+        for (int i = 0; i < primeNum.length; i++) {
+            System.out.printf("%4d",primeNum[i]);
+            if ((i + 1) % 10 == 0){
                 System.out.println();
-
-            System.out.printf("%-4d",primeNumbers[i]);
-        }
-    }
-
-    public static void fillArray(int[] primeNumbers) {
-        int count = 0;
-        int number = 2;
-
-        while (count < primeNumbers.length) {
-            if (isPrime(number, count, primeNumbers)) {
-                primeNumbers[count] = number;
-                count++;
             }
-            number++;
         }
     }
 
-    public static boolean isPrime(int number, int count, int[] primeNumbers) {
-        //It's until count because our array's all elements will be 0 after creating,
-        // but we have to look at our numbers dividable with less prime numbers
-        for (int i = 0; i < count; i++) {
-            // Checking whether the number is dividable with the prime numbers that is less than number
-            if (primeNumbers[i] <= Math.sqrt(number)) {
-                if (number % primeNumbers[i] == 0) {
+    private static void getThePrimeNumbers(int[] primeNum, int NUMBER_OF_PRIMES) {
+        int index = 0;
+        int prime = 2;
+        while (index<NUMBER_OF_PRIMES) {
+
+            if (isPrime(index,prime,primeNum)){
+                primeNum[index] = prime;
+                index++;
+            }
+            prime++;
+        }
+
+    }
+
+    private static boolean isPrime(int index, int prime, int[] primeNum) {
+
+        for (int i = 0; i < index; i++) {
+            if (primeNum[i]<=Math.sqrt(prime)){
+                if (prime%primeNum[i] == 0)
                     return false;
-                }
             }
         }
         return true;
