c09ecf19005995bd527792408b4542322b238c5e
idris <idrisbahce26@gmail.com>
2022-10-22 13:07:53 +0300

    exc

--- Java/src/chapters/chapter08/exercises/Exercise8_30.java
+++ Java/src/chapters/chapter08/exercises/Exercise8_30.java
@@ -6,16 +6,20 @@ public class Exercise8_30 {
     public static void main(String[] args) {
         double[][] arr = getTheArray();
         double[] xandy = linearEquation(arr);
-        System.out.println("x is " + xandy[0] + " and y is" + xandy[1]);
+        if (xandy==null) {
+            System.out.println("The equation has no solution");
+        }else {
+            System.out.println("x is " + xandy[0] + " and y is" + xandy[1]);
+        }
     }
 
     public static double[] linearEquation(double[][] arr) {
         double[] xandy = new double[2];
-        if ((arr[0][0] * arr[1][0]) - (arr[0][1] * arr[1][0]) == 0) {
+        if ((arr[0][0] * arr[1][1]) - (arr[0][1] * arr[1][0]) == 0) {
             return null;
         }
-        xandy[0] = ((arr[2][0] * arr[1][1]) - (arr[2][1] * arr[0][1])) / ((arr[0][0] * arr[1][0]) - (arr[0][1] * arr[1][0]));
-        xandy[1] = ((arr[2][1] * arr[0][0]) - (arr[2][0] * arr[1][0])) / ((arr[0][0] * arr[1][0]) - (arr[0][1] * arr[1][0]));
+        xandy[0] = ((arr[2][0] * arr[1][1]) - (arr[2][1] * arr[0][1])) / ((arr[0][0] * arr[1][1]) - (arr[0][1] * arr[1][0]));
+        xandy[1] = ((arr[2][1] * arr[0][0]) - (arr[2][0] * arr[1][0])) / ((arr[0][0] * arr[1][1]) - (arr[0][1] * arr[1][0]));
         return xandy;
     }
 
---
+++ Java/src/chapters/chapter08/exercises/Exercise8_31.java
@@ -0,0 +1,5 @@
+package chapters.chapter08.exercises;
+
+public class Exercise8_31 {
+
+}
