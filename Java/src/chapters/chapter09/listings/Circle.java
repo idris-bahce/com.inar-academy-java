16a6451f12f26328bb9aae9a84a18bf068e65614
idris <idrisbahce26@gmail.com>
2022-10-24 20:11:23 +0300

    exc

--- Java/src/chapters/chapter09/listings/Circle.java
+++ Java/src/chapters/chapter09/listings/Circle.java
@@ -1,4 +1,19 @@
 package chapters.chapter09.listings;
 
 public class Circle {
+    double radius = 1;
+    Circle(){
+    }
+    Circle(double newRadius){
+        radius = newRadius;
+    }
+    double getArea(){
+        return radius * radius * Math.PI;
+    }
+    double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+    void setRadius(double newRadius){
+        radius = newRadius;
+    }
 }
