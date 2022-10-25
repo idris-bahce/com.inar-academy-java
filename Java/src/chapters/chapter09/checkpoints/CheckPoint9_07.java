4c8d40332cdf8bb413052b299db07ffb03cd1df7
idris <idrisbahce26@gmail.com>
2022-10-25 10:06:52 +0300

    oop

---
+++ Java/src/chapters/chapter09/checkpoints/CheckPoint9_07.java
@@ -0,0 +1,5 @@
+package chapters.chapter09.checkpoints;
+
+public class CheckPoint9_07 {
+    //The member access operator(.) is used to access a data field or invoke a method from an object.
+}
---
+++ Java/src/chapters/chapter09/checkpoints/CheckPoint9_12.java
@@ -0,0 +1,4 @@
+package chapters.chapter09.checkpoints;
+
+public class CheckPoint9_12 {
+}
