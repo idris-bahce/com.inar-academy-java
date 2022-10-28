1809deaa311d3b8bdd093edf53485f232593fdb9
idris <idrisbahce26@gmail.com>
2022-10-28 08:56:23 +0300

    exc

---
+++ Java/src/chapters/chapter09/listings/StaticTest.java
@@ -0,0 +1,21 @@
+package chapters.chapter09.listings;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        Data d1 = new Data();
+        Data d2 = new Data();
+        Data d3 = new Data();
+        Data d4 = new Data();
+        Data d5 = new Data();
+        Data d6 = new Data();
+        Data d7 = new Data();
+        Data d8 = new Data();
+        Data d9 = new Data();
+        Data d10 = new Data();
+        Data d11 = new Data();
+    }
+
+}
+class Data{
+    long[] arr = new long[12_500_000];
+}
