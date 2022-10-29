a45bd71448767037ffdbc08d380fe35cb09c0a5a
idris <idrisbahce26@gmail.com>
2022-10-29 10:22:43 +0300

    exc

---
+++ Java/src/chapters/chapter09/exercises/Exercise9_07/Account.java
@@ -0,0 +1,51 @@
+package chapters.chapter09.exercises.Exercise9_07;
+
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    public Account() {
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(int annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public String getDateCreated() {
+        return getDateCreated().toString();
+    }
+
+    public double getMonthlyInterestRate() {
+        return getAnnualInterestRate() / 1200;
+    }
+
+}
