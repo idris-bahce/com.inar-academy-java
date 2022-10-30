c7bfa0b14ef3bf7d689eb4286a4e10b5972aa1fb
idris <idrisbahce26@gmail.com>
2022-10-30 12:56:36 +0300

    exc

---
+++ Java/src/chapters/chapter09/exercises/Exercise9_09/RegularPolygon.java
@@ -0,0 +1,59 @@
+package chapters.chapter09.exercises.Exercise9_09;
+
+public class RegularPolygon {
+    private int n;
+    private double side;
+    private double x;
+    private double y;
+
+    public RegularPolygon(){
+        this(3,1,0,0);
+    }
+
+    public RegularPolygon(int n, double side, double x, double y) {
+        this.n = n;
+        this.side = side;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getPerimeter(){
+        return n * side;
+    }
+
+    public double getArea(){
+        return (n * side * side) / (4 * Math.tan(Math.PI/n));
+    }
+}
