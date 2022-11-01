92fe491aed2f9c6379eaab9d7c9bff591baf8236
idris <idrisbahce26@gmail.com>
2022-11-01 12:20:24 +0300

    listing

---
+++ Java/src/chapters/chapter10/listings/Loan.java
@@ -0,0 +1,61 @@
+package chapters.chapter10.listings;
+
+import chapters.chapter09.listings.Date;
+
+public class Loan {
+    private double annualInterestRate;
+    private int numberOfYears;
+    private double loanAmount;
+    private Date loanDate;
+
+    public Loan() {
+        this(2.5, 1, 1000);
+    }
+
+    public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
+        this.annualInterestRate = annualInterestRate;
+        this.numberOfYears = numberOfYears;
+        this.loanAmount = loanAmount;
+        loanDate = new Date();
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public int getNumberOfYears() {
+        return numberOfYears;
+    }
+
+    public void setNumberOfYears(int numberOfYears) {
+        this.numberOfYears = numberOfYears;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public double getMonthlyPayment() {
+        double monthlyInterestRate = annualInterestRate / 1200;
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1 -
+                (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+        return monthlyPayment;
+    }
+
+    public double getTotalPayment() {
+        return getMonthlyPayment() * numberOfYears * 12;
+    }
+
+}
