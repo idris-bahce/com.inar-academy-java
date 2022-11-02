0364ee6d4febda7dbeba6a275d3dc7d8fcc21be5
idris <idrisbahce26@gmail.com>
2022-11-02 12:47:36 +0300

    listing

--- Java/src/chapters/chapter10/listings/Course.java
+++ Java/src/chapters/chapter10/listings/Course.java
@@ -11,11 +11,43 @@ public class Course {
     public Course(String courseName) {
         this.courseName = courseName;
     }
-    public void addStudent(String student){
-        students[numberOfStudents]=student;
-        numberOfStudents++;
+
+    public void addStudent(String student) {
+        if (!isCourseFull(students)) {
+            students[numberOfStudents] = student;
+            numberOfStudents++;
+        } else {
+            System.out.println("Course is full." + student + " can not attain this course.");
+        }
     }
-    public void dropStudent(String student){}
+
+    private boolean isCourseFull(String[] students) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void dropStudent(String student) {
+        int indexOfDroppedStudent = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null){
+                continue;
+            }
+            if (students[i].equalsIgnoreCase(student)) {
+                students[i] = null;
+                indexOfDroppedStudent = i;
+                numberOfStudents--;
+            }
+        }
+        for (int i = indexOfDroppedStudent; i < students.length - 1; i++) {//shift the empty index left
+            students[indexOfDroppedStudent] = students[i + 1];
+            indexOfDroppedStudent++;
+        }
+    }
+
     public String getCourseName() {
         return courseName;
     }
