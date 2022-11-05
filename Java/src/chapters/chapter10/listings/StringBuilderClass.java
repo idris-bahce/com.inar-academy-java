cc2823e8ca00007802e71d379a1e230f9d98cf94
idris <idrisbahce26@gmail.com>
2022-11-05 11:54:14 +0300

    oop

--- Java/src/chapters/chapter10/listings/StringBuilderClass.java
+++ Java/src/chapters/chapter10/listings/StringBuilderClass.java
@@ -2,6 +2,13 @@ package chapters.chapter10.listings;
 
 public class StringBuilderClass {
     public static void main(String[] args) {
-        
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Welcome");
+        stringBuilder.append(' ');
+        stringBuilder.append("to");
+        stringBuilder.append(' ');
+        stringBuilder.append("Java");
+
+        System.out.println(stringBuilder);
     }
 }
