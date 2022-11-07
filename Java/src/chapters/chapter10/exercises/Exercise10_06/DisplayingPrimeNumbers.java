8b0901c611b02c13bab117b1f1cef4c86a3a76f3
idris <idrisbahce26@gmail.com>
2022-11-07 11:15:59 +0300

    exc

--- Java/src/chapters/chapter10/exercises/Exercise10_06/DisplayingPrimeNumbers.java
+++ Java/src/chapters/chapter10/exercises/Exercise10_06/DisplayingPrimeNumbers.java
@@ -1,27 +1,38 @@
 package chapters.chapter10.exercises.Exercise10_06;
 
+import chapters.chapter10.listings.StackOfIntegers;
+
 public class DisplayingPrimeNumbers {
     public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
         int primeNumbers = 2;
-        int counter = 0;
-        while (primeNumbers<120){
-            if (isPrime(primeNumbers)){
-                System.out.print(primeNumbers + " ");
-                counter++;
-                if (counter%10==0)
-                    System.out.println();
+        while (primeNumbers < 120) {
+            if (isPrime(primeNumbers)) {
+                stack.push(primeNumbers);
             }
             primeNumbers++;
+        }
+        printPrimeNumbers(stack);
+    }
+
+    private static void printPrimeNumbers(StackOfIntegers stack) {
+        int counter = 0;
+        while (!stack.empty()) {
+            System.out.printf("%4d", stack.pop());
+            counter++;
+            if (counter % 10 == 0) {
+                System.out.println();
+            }
         }
     }
 
     private static boolean isPrime(int primeNumbers) {
-        if (primeNumbers==2){
+        if (primeNumbers == 2) {
             return true;
         }
 
         for (int divider = 2; divider <= primeNumbers / 2; divider++) {
-            if (primeNumbers%divider==0){
+            if (primeNumbers % divider == 0) {
                 return false;
             }
         }
