d22d3a94ee1670d3aced6aa9395d64b179002a05
idris <idrisbahce26@gmail.com>
2022-11-09 11:07:07 +0300

    exc

--- Java/src/chapters/chapter10/exercises/Exercise10_17/FirstTenSquares.java
+++ Java/src/chapters/chapter10/exercises/Exercise10_17/FirstTenSquares.java
@@ -4,10 +4,18 @@ import java.math.BigInteger;
 
 public class FirstTenSquares {
     public static void main(String[] args) {
-        BigInteger big = new BigInteger("" + Long.MAX_VALUE );
+        long maxLongSquareNumber = ((long)Math.sqrt(Long.MAX_VALUE));
+        BigInteger big = new BigInteger("" + maxLongSquareNumber );
+        BigInteger maxLong = new BigInteger("" + Long.MAX_VALUE);
+        System.out.println(maxLong);
+        System.out.println("----------------");
         int counter = 0;
-        while (counter<10){
-            if (big.)
+        while (counter < 10){
+            big = big.add(BigInteger.ONE);
+            if (big.compareTo(maxLong)<0) {
+                System.out.println((counter + 1) + ".-->" + big.multiply(big));
+                counter++;
+            }
         }
     }
 }
