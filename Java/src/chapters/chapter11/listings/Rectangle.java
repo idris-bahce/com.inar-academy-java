7ee8b90335f0146dc93320204d10993240bfc9b8
idris <idrisbahce26@gmail.com>
2022-11-15 10:48:11 +0300

    oop

---
+++ Java/src/chapters/chapter11/listings/Rectangle.java
@@ -0,0 +1,42 @@
+package chapters.chapter11.listings;
+
+public class Rectangle extends GeometricObject{
+    private double width;
+    private double height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getArea(){
+        return width * height;
+    }
+    public double getPerimeter(){
+        return 2 * (width + height);
+    }
+}
