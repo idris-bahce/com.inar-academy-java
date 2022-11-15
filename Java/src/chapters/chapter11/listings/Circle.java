a943f17fb38bfc73becba43db4fa56f5a0467afb
idris <idrisbahce26@gmail.com>
2022-11-15 10:45:08 +0300

    oop

---
+++ Java/src/chapters/chapter11/listings/Circle.java
@@ -0,0 +1,34 @@
+package chapters.chapter11.listings;
+
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+    public double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+    public double getDiameter(){
+        return 2 * radius;
+    }
+}
---
+++ Java/src/chapters/chapter11/listings/GeometricObject.java
@@ -0,0 +1,46 @@
+package chapters.chapter11.listings;
+
+import java.util.Date;
+
+public class GeometricObject {
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject() {
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "GeometricObject{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
+}
