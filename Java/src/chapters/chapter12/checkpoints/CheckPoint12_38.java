2e4c6ef3495a6d9cb50bc434123af8f97356506d
idris <idrisbahce26@gmail.com>
2022-11-22 09:43:23 +0300

    oop

--- Java/src/chapters/chapter12/checkpoints/CheckPoint12_38.java
+++ Java/src/chapters/chapter12/checkpoints/CheckPoint12_38.java
@@ -1,4 +1,5 @@
 package chapters.chapter12.checkpoints;
 
 public class CheckPoint12_38 {
+    //Create a URL object and use new Scanner(url.openStream()) to create a scanner object for reading data from the URL stream.
 }
