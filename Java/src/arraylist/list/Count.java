dfcecbfa474bc9f0ee094f7acdc049a9e7a7c4d7
idris <idrisbahce26@gmail.com>
2022-12-05 22:02:01 +0300

    exc

---
+++ Java/src/arraylist/list/Count.java
@@ -0,0 +1,34 @@
+package arraylist.list;
+
+import java.util.ArrayList;
+
+public class Count {
+    public static void main(String[] args) {
+        ArrayList<Integer> randomList = getList();
+        ArrayList<Integer> count = fillTheArray();
+        countTheRandomList(count,randomList);
+        System.out.println(count);
+    }
+
+    private static ArrayList<Integer> fillTheArray() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(0);
+        }
+        return list;
+    }
+
+    private static void countTheRandomList(ArrayList<Integer> count, ArrayList<Integer> randomList) {
+        for (int i = 0; i < randomList.size(); i++) {
+            count.set(randomList.get(i),count.get(randomList.get(i)) + 1);
+        }
+    }
+
+    private static ArrayList<Integer> getList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add((int) (Math.random() * 10));
+        }
+        return list;
+    }
+}
