5546c64bf6ca91fb8f26a7dc076b4ff1409c6280
idris <idrisbahce26@gmail.com>
2022-12-06 21:52:10 +0300

    exc

--- Java/src/homework/set/DistinctNumbers.java
+++ Java/src/homework/set/DistinctNumbers.java
@@ -1,4 +1,18 @@
 package homework.set;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class DistinctNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter ten numbers: ");
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            set.add(in.nextInt());
+        }
+        System.out.println("The number of distinct number is " + set.size());
+        System.out.println(set);
+    }
 }
