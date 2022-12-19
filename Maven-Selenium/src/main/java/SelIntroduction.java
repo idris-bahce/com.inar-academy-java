47d5c60310741edbaea2a27d4f65835ea184afb4
idris <idrisbahce26@gmail.com>
2022-12-19 11:25:04 +0300

    maven

--- Maven-Selenium/src/main/java/SelIntroduction.java
+++ Maven-Selenium/src/main/java/SelIntroduction.java
@@ -1,12 +1,20 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelIntroduction {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://github.com/");
+     //  System.setProperty("webdriver.chrome.driver","C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
+     //  WebDriver driver = new ChromeDriver();
+     //  driver.get("https://github.com/");
+     //  System.out.println(driver.getTitle());
+     //  driver.close();
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\idris\\Desktop\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.wikipedia.org/");
+        System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-        driver.close();
+        driver.quit();
     }
 }
