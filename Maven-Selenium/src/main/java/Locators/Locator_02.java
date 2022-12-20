8dea954d692cb6e24275cd1619c6ff4ee8f397da
idris <idrisbahce26@gmail.com>
2022-12-20 08:18:08 +0300

    exc

--- Maven-Selenium/src/main/java/Locators/Locator_02.java
+++ Maven-Selenium/src/main/java/Locators/Locator_02.java
@@ -20,6 +20,14 @@ public class Locator_02 {
                 driver.findElement(By.linkText("Forgot your password?")).click();
                 Thread.sleep(3000);
                 driver.findElement(By.xpath("//button[@class='reset-pwd-btn']")).click();
+                Thread.sleep(3000);
+                String s = driver.findElement(By.xpath("//p[@class='infoMsg']")).getText();
+                String[] strings = s.split("'");
+                driver.findElement(By.cssSelector("button.go-to-login-btn")).click();
+                driver.findElement(By.id("inputUsername")).sendKeys("idris");
+                driver.findElement(By.name("inputPassword")).sendKeys(strings[1]);
+                Thread.sleep(2000);
+                driver.findElement(By.className("signInBtn")).click();
             }
         } catch (Exception a) {
            driver.quit();
