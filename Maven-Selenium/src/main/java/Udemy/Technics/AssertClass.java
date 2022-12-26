7fca67ac63e1a6715b0ebb4e0fcacfa27762d70b
idris <idrisbahce26@gmail.com>
2022-12-26 09:05:08 +0300

    exc

---
+++ Maven-Selenium/src/main/java/Udemy/Technics/AssertClass.java
@@ -0,0 +1,21 @@
+package Udemy.Technics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class AssertClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+    }
+}
