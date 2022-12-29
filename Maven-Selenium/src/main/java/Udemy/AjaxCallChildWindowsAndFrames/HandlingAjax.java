11139bfe909409e40c0f7559df0603fa3e588c24
idris <idrisbahce26@gmail.com>
2022-12-29 09:55:34 +0300

    exc

---
+++ Maven-Selenium/src/main/java/Udemy/AjaxCallChildWindowsAndFrames/HandlingAjax.java
@@ -0,0 +1,25 @@
+package Udemy.AjaxCallChildWindowsAndFrames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class HandlingAjax {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        //move mouse to the location
+        actions.moveToElement(driver.findElement(By.id("nav-link-accountList-nav-line-1"))).build().perform();
+        //entering letters with capital
+        actions.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+    }
+}
