35f6238668d79d2f2068d0a708785ca00bf6568f
idris <idrisbahce26@gmail.com>
2022-12-29 23:33:21 +0300

    exc

--- Maven-Selenium/src/main/java/Udemy/AjaxCallChildWindowsAndFrames/ChildBrowser.java
+++ Maven-Selenium/src/main/java/Udemy/AjaxCallChildWindowsAndFrames/ChildBrowser.java
@@ -1,16 +1,29 @@
 package Udemy.AjaxCallChildWindowsAndFrames;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ChildBrowser {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://www.amazon.com/");
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
         driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String childId = ";";
+        while (it.hasNext()){
+            childId = it.next();
+        }
+        driver.switchTo().window(childId);
+        driver.findElement(By.cssSelector(".im-para.red")).getText();
     }
 }
