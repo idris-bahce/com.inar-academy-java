d2ee95f7bd59f79782ebbd5b499f3780b3cb5043
idris <idrisbahce26@gmail.com>
2022-12-31 09:19:07 +0300

    exc

--- Maven-Selenium/src/main/java/Udemy/PracticalProblems/JavaScriptExecutorDemo.java
+++ Maven-Selenium/src/main/java/Udemy/PracticalProblems/JavaScriptExecutorDemo.java
@@ -1,10 +1,13 @@
 package Udemy.PracticalProblems;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class JavaScriptExecutorDemo {
     public static void main(String[] args) {
@@ -17,5 +20,12 @@ public class JavaScriptExecutorDemo {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,550)");
         js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=500");
+        //Handling Table Grids
+        int sum =0;
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='tableFixHead']//td[4]"));
+        for (WebElement element: elements ) {
+            sum += Integer.parseInt(element.getText());
+        }
+        System.out.println("Sum of the numbers in table is: " + sum);
     }
 }
