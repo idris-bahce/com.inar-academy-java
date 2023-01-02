c5ffe33b740e2a6a75485a217540a88c1973958e
idris <idrisbahce26@gmail.com>
2023-01-02 23:03:00 +0300

    exc

--- Maven-Selenium/src/main/java/Udemy/Streams/SortedList.java
+++ Maven-Selenium/src/main/java/Udemy/Streams/SortedList.java
@@ -1,9 +1,15 @@
 package Udemy.Streams;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortedList {
     public static void main(String[] args) {
@@ -13,6 +19,10 @@ public class SortedList {
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
         driver.manage().window().maximize();
 
-        
+        driver.findElement(By.xpath("//tr/th[1]")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//tr/td[1]"));
+        List<String> originalList = elements.stream().map(s->s.getText()).collect(Collectors.toList());
+        List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
+        Assert.assertEquals(originalList,sortedList);
     }
 }
