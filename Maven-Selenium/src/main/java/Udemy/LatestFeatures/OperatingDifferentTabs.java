7fbfa736c99fc56a6dba6a0333a193bdfc60baa3
idris <idrisbahce26@gmail.com>
2023-01-04 08:07:30 +0300

    exc

--- Maven-Selenium/src/main/java/Udemy/LatestFeatures/OperatingDifferentTabs.java
+++ Maven-Selenium/src/main/java/Udemy/LatestFeatures/OperatingDifferentTabs.java
@@ -1,16 +1,17 @@
 package Udemy.LatestFeatures;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
 
 public class OperatingDifferentTabs {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\idris\\Desktop\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
@@ -26,7 +27,10 @@ public class OperatingDifferentTabs {
         driver.get("https://rahulshettyacademy.com/");
         String nameOfCourse = driver.findElement(By.xpath("(//a[@href='https://courses.rahulshettyacademy.com/p/core-java-for-automation-testers-interview-programs'])[2]")).getText();
         driver.switchTo().window(parentURL);
-        driver.findElement(By.xpath("(//input[@name='name'][1])[1]")).sendKeys(nameOfCourse);
+        WebElement nameBox = driver.findElement(By.xpath("(//input[@name='name'][1])[1]"));
+        nameBox.sendKeys(nameOfCourse);
+        File file = nameBox.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file,new File("logo.png"));
 
     }
 }
